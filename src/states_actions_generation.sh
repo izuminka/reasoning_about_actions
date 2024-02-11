16c1b2335310c0ea677f38bd0add97ea3990133f
[ Divij Handa - Sch Compt Dhanda Augmented Intellig - Graduate Student - dhanda ] <dhanda@login01.sol.rc.asu.edu>
2024-02-11 15:11:38 -0700

    Added state_changes

---
+++ src/states_actions_generation.sh
@@ -0,0 +1,25 @@
+DOMAINS=('blocksworld' 'depots' 'driverlog' 'goldminer' 'grippers' 'logistics' 'miconic' 'mystery' 'npuzzle' 'satellite' 'spanner' 'visitall' 'zenotravel')
+INSTANCES=('Instance_1' 'Instance_2' 'Instance_3' 'Instance_4' 'Instance_5' 'Instance_6' 'Instance_7' 'Instance_8' 'Instance_9' 'Instance_10')
+
+PYTHON_CMD='python states_actions_generation.py'
+
+for domain in "${DOMAINS[@]}"; do
+    for instance in "${INSTANCES[@]}"; do
+        sbatch_file_name="sbatch_{$domain}_{$instance}.sh"
+        echo '#!/bin/bash' > $sbatch_file_name
+        echo '#SBATCH -N 1' >> $sbatch_file_name
+        echo '#SBATCH -c 2' >> $sbatch_file_name
+        echo '#SBATCH -t 4-00:00:00' >> $sbatch_file_name
+        echo '#SBATCH -p general' >> $sbatch_file_name
+        echo '#SBATCH -q public' >> $sbatch_file_name
+        echo '#SBATCH --mail-type=NONE' >> $sbatch_file_name
+        echo '#SBATCH --export=NONE' >> $sbatch_file_name
+        echo 'module purge' >> $sbatch_file_name
+        echo 'module load mamba/latest' >> $sbatch_file_name
+        echo 'source activate reasoning_about_actions' >> $sbatch_file_name
+        echo "$PYTHON_CMD -d $domain -i $instance" >> $sbatch_file_name
+        sbatch $sbatch_file_name
+        echo "Successfully submitted {$domain} {$instance}"
+        rm $sbatch_file_name
+    done
+done
