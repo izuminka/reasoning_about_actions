24384b0da97f64a24593744da9fc047e9d89a346
[ Divij Handa - Sch Compt Dhanda Augmented Intellig - Graduate Student - dhanda ] <dhanda@login01.sol.rc.asu.edu>
2024-02-11 15:18:30 -0700

    Added states_actions

--- src/states_actions_generation.sh
+++ src/states_actions_generation.sh
@@ -1,11 +1,11 @@
-DOMAINS=('blocksworld' 'depots' 'driverlog' 'goldminer' 'grippers' 'logistics' 'miconic' 'mystery' 'npuzzle' 'satellite' 'spanner' 'visitall' 'zenotravel')
+DOMAINS=('zenotravel')
 INSTANCES=('Instance_1' 'Instance_2' 'Instance_3' 'Instance_4' 'Instance_5' 'Instance_6' 'Instance_7' 'Instance_8' 'Instance_9' 'Instance_10')
 
 PYTHON_CMD='python states_actions_generation.py'
 
 for domain in "${DOMAINS[@]}"; do
     for instance in "${INSTANCES[@]}"; do
-        sbatch_file_name="sbatch_{$domain}_{$instance}.sh"
+        sbatch_file_name="sbatch_${domain}_${instance}.sh"
         echo '#!/bin/bash' > $sbatch_file_name
         echo '#SBATCH -N 1' >> $sbatch_file_name
         echo '#SBATCH -c 2' >> $sbatch_file_name
@@ -19,7 +19,7 @@ for domain in "${DOMAINS[@]}"; do
         echo 'source activate reasoning_about_actions' >> $sbatch_file_name
         echo "$PYTHON_CMD -d $domain -i $instance" >> $sbatch_file_name
         sbatch $sbatch_file_name
-        echo "Successfully submitted {$domain} {$instance}"
+        echo "Successfully submitted ${domain} ${instance}"
         rm $sbatch_file_name
     done
 done
