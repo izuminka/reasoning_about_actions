626ccc6de3662f2ecbc6ed7630b02d323d721717
Shrinidhi Kumbhar <shri13081998@gmail.com>
2024-02-14 01:46:13 -0800

    pushed zero shot data

--- src/evaluation/prompting/data_gen.py
+++ src/evaluation/prompting/data_gen.py
@@ -15,20 +15,44 @@ from promping_stencils import Generate_prompting_template
 from src.questions_construction.domains import *
 
 
+
 root_directory = '/data_4/data/shri/reasoning_about_actions/data/questions/'
-domain_class = Blocksworld()
-domain_name = 'blocksworld'
+domain_class = Visitall()
+domain_name = 'visitall'
 instance_id = 1
-with open(f'/data_4/data/shri/reasoning_about_actions/data/questions/blocksworld/Instance_1.jsonl', 'r') as f:
+with open(f'/data_4/data/shri/reasoning_about_actions/data/questions/visitall/Instance_1.jsonl', 'r') as f:
     data = f.readlines()
 unique_instance_dict = [json.loads(x) for x in data]
 
-def zero_shot_data_gen(root_directory, domain_class, instance_id, domain_name, unique_instance_dict):
+def zero_shot_data_gen(root_directory, domain_class, instance_id, domain_name,unique_instance_dict):
+    set_of_unique_instances = set()
+    data = []
     for i in range(len(unique_instance_dict)):
-        prompt = Generate_prompting_template(root_directory, domain_class, instance_id, domain_name+'/',unique_instance_dict[i]).zero_shot_prompt()
-        unique_instance_dict[i]['prompt']= prompt[0]['zero_shot_model_input']
-    df = pd.DataFrame(unique_instance_dict).T
-    return df.to_csv('/data_4/data/shri/reasoning_about_actions/data/data_files/'+domain_name+'_zero_shot_prompt.csv')
+        if (unique_instance_dict[i]['plan_length'],unique_instance_dict[i]['question_name'],unique_instance_dict[i]['question_category'],unique_instance_dict[i]['answer_type']) not in set_of_unique_instances:
+            set_of_unique_instances.add((unique_instance_dict[i]['plan_length'], unique_instance_dict[i]['question_name'], unique_instance_dict[i]['question_category'], unique_instance_dict[i]['answer_type']))
+            zero_shot_prompt = Generate_prompting_template(root_directory, domain_class, instance_id, domain_name+'/',unique_instance_dict[i]).zero_shot_prompt()
+            unique_instance_dict[i]['prompt'] = zero_shot_prompt[0]['zero_shot_model_input']
+            data.append(unique_instance_dict[i])
+        else:
+            continue    
+        
+    #save data to jsonl
+    with open(f'/data_4/data/shri/reasoning_about_actions/data/data_files/zero_shot_data/{domain_name}.jsonl', 'w') as f:
+        for item in data:
+            f.write(json.dumps(item))
+            f.write('\n')
+    
+    
+    
 
 
-zero_shot_data_gen(root_directory, domain_class, instance_id, domain_name, unique_instance_dict)
+zero_shot_data_gen(root_directory, domain_class, instance_id, domain_name,unique_instance_dict)
+# with open('/data_4/data/shri/reasoning_about_actions/data/final_data/zero_shot_data/blocksworld.jsonl', 'r') as f:
+#     data = f.readlines()
+# unique_instance_dict = [json.loads(x) for x in data]
+# results = defaultdict(int)
+# for item in unique_instance_dict:
+#     if item['plan_length'] == 1:
+#         results[(item['question_category'],item['question_name'],item['answer'].split(' ')[0])] += 1
+# for k,v in results.items():
+#     print(k,v)
