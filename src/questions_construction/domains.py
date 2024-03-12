b321009519bd4dceb85d43f4318c49112b18b6fe
izuminka <pavel.a.dolin@gmail.com>
2024-03-12 15:14:03 -0700

    clean ups

--- src/questions_construction/domains.py
+++ src/questions_construction/domains.py
@@ -72,7 +72,7 @@ class BaseDomain:
             nl_fluent = self.fluent_to_hallucinated_natural_language_helper(fluent)
 
         if self.is_random_sub:
-            return self.replace_substrings(nl_fluent, self.ALL_TO_RAND)
+            return self.replace_substring(nl_fluent, self.ALL_TO_RAND)
         else:
             return nl_fluent
 
@@ -83,7 +83,7 @@ class BaseDomain:
             nl_action = self.action_to_hallucinated_natural_language_helper(action)
 
         if self.is_random_sub:
-            return self.replace_substrings(nl_action, self.ALL_TO_RAND)
+            return self.replace_substring(nl_action, self.ALL_TO_RAND)
         else:
             return nl_action
 
