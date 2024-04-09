ccfcca049a80fcecde8177f93e5a1b62bc589bc9
izuminka <pavel.a.dolin@gmail.com>
2024-04-08 18:54:25 -0700

    rm old tests, fix tests

--- src/tests/test_domains.py
+++ src/tests/test_domains.py
@@ -1,6 +1,8 @@
 from src.questions_construction.domains import *
 import unittest
 
+random.seed(42)
+
 
 class TestDomains(unittest.TestCase):
     bd = BaseDomain(False, True)
@@ -51,13 +53,13 @@ class TestDomains(unittest.TestCase):
         obj_dict = Blocksworld.SUBSTRINGS_TO_RAND
         res = self.bd.replace_substrings(text, obj_dict)
         print(res)
-        expected = ('Ovyuecllio a qbyyxzqvdh is only possible if that qbyyxzqvdh is clear, on the '
-                    'zewwtdxhfs, and the egpbpdtalq is empty. Wxqdwukszo the first qbyyxzqvdh '
-                    'from the second causes first qbyyxzqvdh to be casqqrrojp a qbyyxzqvdh is '
-                    'said to be clear if it is not being casqqrrojp and there are no qbyyxzqvdhs '
-                    'that are on top of it. The egpbpdtalq is said to be empty if and only if it '
-                    'is not casqqrrojp any qbyyxzqvdh. The qbyyxzqvdh can only be at one place at '
-                    'a time.')
+        expected = ('Ovyuecllio a qbyyxzqvdh is only possible if that qbyyxzqvdh is clear, '
+                    'wtuwjwbuja the gcbwvwyvkv, and the egpbpdtalq is empty. Wxqdwukszo the first '
+                    'qbyyxzqvdh from the second causes first qbyyxzqvdh to be casqqrrojp a '
+                    'qbyyxzqvdh is said to be clear if it is not being casqqrrojp and there are '
+                    'no qbyyxzqvdhs that are wtuwjwbuja top of it. The egpbpdtalq is said to be '
+                    'empty if and only if it is not casqqrrojp any qbyyxzqvdh. The qbyyxzqvdh can '
+                    'only be at one place at a time.')
         self.assertEqual(expected, res)
 
     def test_domains_init(self):
@@ -83,20 +85,32 @@ class TestDomains(unittest.TestCase):
     def test_blocksworld_domain_description(self):
         dom = ALL_DOMAIN_CLASSES[0]
         domain_class = dom(False, True)
-        expected_text = (
-            'Picking up a block is only possible if that block is clear, on the table, and the hand is empty. '
-            'Picking up the block leads to the block being held. Putting down the block can only be executed if the block is being held. '
-            'Putting down the block causes the block to be on the table. A block can be stacked on the second block if it is being held and the second block is clear. '
-            'By stacking the first block on the second, it causes the first block to be on top of the second block. '
-            'The block can also be unstacked from the top of the second block only if the hand is empty and the first block is clear and on top of the second block. '
-            'Unstacking the first block from the second causes first block to be held A block is said to be clear if it is not being held and there are no blocks that are on top of it. '
-            'The hand is said to be empty if and only if it is not holding any block. The block can only be at one place at a time.\n\n'
-
-            'A state is a set of valid properties. Properties may or may not involve negations. Properties of the state can be of 4 types: base, derived, persistent, and static. '
-            "Base properties of the state are properties that don't depend on other properties. In this domain, they are: on the table and not on the table. "
-            'Derived properties of the state are properties that depend on other properties. In this domain, they are: clear, hand is empty, not clear and hand is not empty. '
-            'Self constraint properties of the state are properties that depend on themselves. In this domain, they are: holding, on, not holding and not on. '
-            "Static properties of the state are properties that don't change under any action. There are no static properties of the state in this domain. ")
+        expected_text = ('Picking up a block is only possible if that block is clear, on the table, '
+                         'and the hand is empty. Picking up the block leads to the block being held. '
+                         'Putting down the block can only be executed if the block is being held. '
+                         'Putting down the block causes the block to be on the table. A block can be '
+                         'stacked on the second block if it is being held and the second block is '
+                         'clear. By stacking the first block on the second, it causes the first block '
+                         'to be on top of the second block. The block can also be unstacked from the '
+                         'top of the second block only if the hand is empty and the first block is '
+                         'clear and on top of the second block. Unstacking the first block from the '
+                         'second causes first block to be held A block is said to be clear if it is '
+                         'not being held and there are no blocks that are on top of it. The hand is '
+                         'said to be empty if and only if it is not holding any block. The block can '
+                         'only be at one place at a time.\n'
+                         '\n'
+                         'A state is a set of valid properties. Properties may or may not involve '
+                         'negations. Properties of the state can be of 4 types: base, derived, '
+                         'persistent, and static. Base properties of the state are properties that '
+                         "don't depend on other properties. In this domain, they are: a block is on "
+                         'the table and a block is not on the table. Derived properties of the state '
+                         'are properties that depend on other properties. In this domain, they are: a '
+                         'block is clear, hand is empty, a block is not clear and hand is not empty. '
+                         'Self constraint properties of the state are properties that depend on '
+                         'themselves. In this domain, they are: a block is being held, a block is on '
+                         'another block, a block is not being held and a block is not on another '
+                         "block. Static properties of the state are properties that don't change under "
+                         'any action. There are no static properties of the state in this domain. ')
 
         self.assertEqual(expected_text, domain_class.domain_description)
 
