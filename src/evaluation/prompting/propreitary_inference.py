41f4fd055ff6e1a07a1d602bfd5d652442a7e952
izuminka <pavel.a.dolin@gmail.com>
2024-05-27 17:41:47 -0700

    prompt eval propr. fix

--- src/evaluation/prompting/propreitary_inference.py
+++ src/evaluation/prompting/propreitary_inference.py
@@ -1,9 +1,11 @@
+import time
+
 from tqdm import tqdm
 import json
 from pathlib import Path
 import argparse
 import google.generativeai as genai    # pip install -q -U google-generativeai
-from openai import OpenAI              # pip install openai
+import openai           # pip install openai
 
 GEMINI_MODEL_NAME = 'gemini-pro'
 GPT_MODEL_NAME = 'gpt-4o' #'gpt-4-0125-preview'
@@ -13,6 +15,16 @@ GPT_MODEL_NAME = 'gpt-4o' #'gpt-4-0125-preview'
 # model = genai.get_model(f'models/{GEMINI_MODEL_NAME}') # get model info
 INPUT_TOKEN_LIMIT = 30720
 OUTPUT_TOKEN_LIMIT = 2048
+
+REGEX_TIME = r"in (\d+\.\d+)s"
+import re
+
+def get_try_time(pattern, default=60):
+    match = re.search(pattern, REGEX_TIME)
+    if match:
+        return match.group(1)*2
+    else:
+        return default
 
 
 def get_response(text, model_name, model=None, temp=0.0):
@@ -45,7 +57,7 @@ def get_response(text, model_name, model=None, temp=0.0):
         except:
             return "NO RESPONSE"
     elif model_name == GPT_MODEL_NAME:
-        response = client.ChatCompletion.create(
+        response = openai.ChatCompletion.create(
             model = GPT_MODEL_NAME,
             messages = [
                 {"role": "user", "content": text}
@@ -79,10 +91,11 @@ def parse_args():
     parser.add_argument('-m', '--model', type=str, required=True, help='Model name')
     parser.add_argument('-f', '--file', type=str, required=True, help='Input file path')
     parser.add_argument('-o', '--output', type=str, required=True, help='Output file path')
-    parser.add_argument('-k', '--model_api_key', type=str, required=True, help='API key')
+    # parser.add_argument('-k', '--model_api_key', type=str, required=True, help='API key')
     parser.add_argument('-i', '--index', type=int, required=False, help='Starting zero-index for evaluation')
     parser.add_argument('-t', '--temperature', type=float, required=False, default=0.0, help='Temperature for generation')
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     # Getting arguments
@@ -94,7 +107,8 @@ if __name__ == '__main__':
         model_name = GEMINI_MODEL_NAME
         model = genai.GenerativeModel(model_name)
     elif args.model.lower()=='gpt4' or args.model.lower()=='gpt' or args.model.lower()=='gpt-4':
-        client = OpenAI(api_key=args.model_api_key)
+        with open('openai.api.key') as f:
+            openai.api_key = f.read()
         model_name = GPT_MODEL_NAME
     else:
         raise Exception(f'{args.model} is an invalid model')
@@ -113,22 +127,30 @@ if __name__ == '__main__':
     # Prompting the model
     with tqdm(total=len(data)) as pbar:
         for idx, ele in enumerate(data):
-            if model_name == GEMINI_MODEL_NAME:
-                response = get_response(
-                    text = ele['prompt'],
-                    model_name = model_name,
-                    model = model,
-                    temp = args.temperature
-                )
-            elif model_name == GPT_MODEL_NAME:
-                response = get_response(
-                    text = ele['prompt'],
-                    model_name = model_name,
-                    temp = args.temperature
-                )
-            
+            num_tries=5
+            while num_tries > 0:
+                try:
+                    if model_name == GEMINI_MODEL_NAME:
+                        response = get_response(
+                            text = ele['prompt'],
+                            model_name = model_name,
+                            model = model,
+                            temp = args.temperature
+                        )
+                    elif model_name == GPT_MODEL_NAME:
+                        response = get_response(
+                            text = ele['prompt'],
+                            model_name = model_name,
+                            temp = args.temperature
+                        )
+                    break
+                except openai.error.RateLimitError as e:
+                    print(e)
+                    backoff_time = get_try_time(e)
+                    print(f"Rate limit exceeded. Backing off for {backoff_time} seconds...")
+                    time.sleep(backoff_time)
+                    num_tries -= 1
             ele['response'] = response
             print(response)
-            exit()
             write_response(ele, args.output)
             pbar.update(1)
---
+++ src/evaluation/prompting/propreitary_inference_mod.sh
@@ -0,0 +1,59 @@
+MODELS=(
+    'gpt4'
+)
+DOMAINS=(
+    'blocksworld'
+    'depots'
+    'driverlog'
+    'goldminer'
+    'grippers'
+    'logistics'
+    'miconic'
+    'mystery'
+    'npuzzle'
+    'satellite'
+    'spanner'
+    'visitall'
+    'zenotravel'
+)
+PROMPTS=(
+    'few_shot_1'
+    'few_shot_5'
+)
+
+INSTANCES=(
+  'Instance_1'
+  'Instance_2'
+  'Instance_3'
+  'Instance_4'
+  'Instance_5'
+  'Instance_6'
+  'Instance_7'
+  'Instance_8'
+  'Instance_9'
+  'Instance_10'
+)
+
+
+SUBS_RAMS_EXT='without_random_sub/without_ramifications'
+INPUT_FILE='../../../data/data_for_test/'$SUBS_RAMS_EXT
+OUTPUT_FILE='../../../results'
+#INDEX=0
+#TEMPERATURE=0.0
+
+for model in "${MODELS[@]}"; do
+    for domain in "${DOMAINS[@]}"; do
+        for instance in "${INSTANCES[@]}"; do
+          for prompt in "${PROMPTS[@]}"; do
+              sbatch_file_name="sbatch_${model}_${domain}_${prompt}_${instance}.sh"
+              echo "python propreitary_inference.py \
+-m ${model} \
+-f ${INPUT_FILE}/${prompt}/${domain}/${instance}.jsonl \
+-o ${OUTPUT_FILE}/${model}/${SUBS_RAMS_EXT}/${prompt}/${domain}/${instance}.jsonl
+" >> $sbatch_file_name
+            echo "Successfully submitted ${model} ${domain} ${prompt} ${instance}"
+#            rm $sbatch_file_name
+        done
+    done
+done
+done
